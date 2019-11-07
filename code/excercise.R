43dc7a4f3e70e1a4bcb07d74960aa4d9da1d571f
Patrick Weigelt <pweigel@uni-goettingen.de>
2019-11-07 16:40:31 +0100

    added tidy version for excercise

--- code/excercise.R
+++ code/excercise.R
@@ -1,12 +1,7 @@
 ### excercise: Community weighted means
 
-
-
-
 head(traits_FD)
 specsbysites_ab[1:5,1:5]
-
-names(specsbysites_ab) <- gsub("_"," ",names(specsbysites_ab))
 
 all(row.names(traits_FD)==names(specsbysites_ab))
 
@@ -20,6 +15,7 @@ sum(traits_FD[,"MaxStemHeight_m"] * specsbysites_ab["AB0081",], na.rm = TRUE)/su
 # loop: 
 CMWs <- matrix(ncol = ncol(traits_FD), nrow = nrow(specsbysites_ab), dimnames=list(rownames(specsbysites_ab), colnames(traits_FD)))
 for (i in 1:ncol(CMWs)){
+  print(i)
   for (k in 1:nrow(CMWs)){
     CMWs[k,i] <- sum(traits_FD[,i] * specsbysites_ab[k,], na.rm = TRUE)/sum(specsbysites_ab[k,], na.rm = TRUE)
   }
@@ -37,7 +33,20 @@ cmw(traits_FD[,"MaxStemHeight_m"],specsbysites_ab["AB0081",])
 # apply the function
 apply(traits_FD, 2, function(x) cmw(x, specsbysites_ab["AB0081",]))
 
+palms_PD <- pd.query(palm_tree_pruned, specsbysites_ab, standardize = FALSE) 
+
 CMWs_applied <- t(apply(specsbysites_ab, 1, function(x) apply(traits_FD, 2, function(y) cmw(y, x))))
+
+# Tidy version
+specsbysites_ab$site= row.names(specsbysites_ab)
+traits_FD$species= row.names(traits_FD)
+
+CWM_df <- gather(specsbysites_ab, species, abundance, -site) %>%
+  inner_join(traits_FD, by='species') %>%
+  gather(trait, value, -site, -species, -abundance) %>%
+  group_by(site, trait) %>%
+  summarise(CWM=weighted.mean(value, abundance))
+
 
 
 
